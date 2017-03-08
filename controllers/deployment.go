ea17784f8b1ca12721c799c4c4bf6ba1983b5452
xwisen <1031649164@qq.com>
2017-03-08 17:49:58 +0800

    Add create deployment

--- controllers/deployment.go
+++ controllers/deployment.go
@@ -6,17 +6,17 @@
 //        Author: xwisen 1031649164@qq.com
 //   Description: ---
 //        Create: 2017-03-07 10:36:33
-// Last Modified: 2017-03-08 13:55:22
+// Last Modified: 2017-03-08 17:48:43
 //***********************************************
 
 package controllers
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	"github.com/astaxie/beego/logs"
-	metav1 "k8s.io/client-go/pkg/api/v1"
-	//"k8s.io/client-go/tools/clientcmd"
 	"github.com/xwisen/mtk8/client"
+	metav1 "k8s.io/client-go/pkg/api/v1"
+	extensionsV1beta1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
 type DeploymentController struct {
@@ -25,15 +25,26 @@ type DeploymentController struct {
 }
 
 func (dc *DeploymentController) CreateDeployment() {
-	dc.Ctx.WriteString("CreateDeployment handler\n")
+	d := extensionsV1beta1.Deployment{}
+	err := json.Unmarshal(dc.Ctx.Input.RequestBody, &d)
+	if err != nil {
+		logs.Error("parse request body err: %v", err)
+	}
+	//logs.Info("%v", &d)
+	resp, err := client.K8sDefaultClient.ExtensionsV1beta1().Deployments("default").Create(&d)
+	if err != nil {
+		logs.Error("create deployment response err: %v", err)
+		dc.Data["json"] = err.Error()
+	} else {
+		dc.Data["json"] = &resp
+	}
+	dc.ServeJSON()
 }
 
 func (dc *DeploymentController) DeleteDeployment() {
-	dc.Ctx.WriteString("DeleteDeployment handler\n")
 }
 
 func (dc *DeploymentController) PatchDeployment() {
-	dc.Ctx.WriteString("PatchDeployment handler\n")
 }
 
 func (dc *DeploymentController) ListDeployment() {
