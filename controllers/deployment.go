425a56ca19b4121d5795a848f3025cefc305b4b9
xwisen <1031649164@qq.com>
2017-05-26 08:43:02 +0800

    Add dp-rs-pod lists

--- controllers/deployment.go
+++ controllers/deployment.go
@@ -6,17 +6,19 @@
 //        Author: xwisen 1031649164@qq.com
 //   Description: ---
 //        Create: 2017-03-07 10:36:33
-// Last Modified: 2017-05-23 09:59:00
+// Last Modified: 2017-05-25 23:19:12
 //***********************************************
 
 package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"github.com/xwisen/mtk8/client"
 	//metav1 "k8s.io/client-go/pkg/api/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//labels "k8s.io/apimachinery/pkg/labels"
 	extensionsV1beta1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
@@ -62,10 +64,27 @@ func (dc *DeploymentController) PatchDeployment() {
 }
 
 func (dc *DeploymentController) ListDeployment() {
-	depolymentLists, err := client.K8sDefaultClient.ExtensionsV1beta1().Deployments("").List(metav1.ListOptions{})
+	deploymentLists, err := client.K8sDefaultClient.ExtensionsV1beta1().Deployments("").List(metav1.ListOptions{})
 	if err != nil {
 		logs.Error("list deployment error : %v", err)
 	}
-	dc.Data["json"] = &depolymentLists
+	for did, dp := range deploymentLists.Items {
+		logs.Info("deployment %s in %s,id is %d", dp.GetName(), dp.GetNamespace(), did)
+		rsLists, err := client.K8sDefaultClient.ExtensionsV1beta1().ReplicaSets("").List(metav1.ListOptions{LabelSelector: fmt.Sprintf("run=%s", dp.Spec.Selector.MatchLabels["run"])})
+		if err != nil {
+			logs.Error("list rs error : %v", err)
+		}
+		for rid, rs := range rsLists.Items {
+			logs.Info("rs %s in %s,id is %d,belong to dp %s", rs.GetName(), rs.GetNamespace(), rid, dp.GetName())
+			podLists, err := client.K8sDefaultClient.CoreV1().Pods("").List(metav1.ListOptions{LabelSelector: fmt.Sprintf("pod-template-hash=%s", rs.Spec.Selector.MatchLabels["pod-template-hash"])})
+			if err != nil {
+				logs.Error("list pod error : %v", err)
+			}
+			for pid, pod := range podLists.Items {
+				logs.Info("pod %s in %s ,id is %d,belong to rs %s", pod.GetName(), pod.GetNamespace(), pid, rs.GetName())
+			}
+		}
+	}
+	dc.Data["json"] = &deploymentLists
 	dc.ServeJSON()
 }
